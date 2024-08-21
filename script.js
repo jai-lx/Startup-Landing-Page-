93166daa4c7cd41c9711ab6684a62b4746d75102
jai-lx <jaideeppuri676@gmail.com>
2024-08-21 10:40:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+// script.js
+// Placeholder for any interactive JavaScript you may want to add
+// Example: Smooth scroll for anchor links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
